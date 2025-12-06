dbf07dea7ad866deea2c1d38ff2e8362ec935fdd
Pfalzventure <robertprokasky@t-online.de>
2025-12-06 23:03:34 +0100

    Update mollieClient.ts

--- mollieClient.ts
+++ mollieClient.ts
@@ -1,4 +1,5 @@
 import createMollieClient from "@mollie/api-client";
+console.log("Mollie API Key:", process.env.MOLLIE_API_KEY);
 
 export const mollie = createMollieClient({
   apiKey: process.env.MOLLIE_API_KEY!
