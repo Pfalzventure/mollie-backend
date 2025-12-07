4233f5b0d4a8ed65042ad49fadee1549778aac94
Pfalzventure <robertprokasky@t-online.de>
2025-12-07 10:19:11 +0100

    Update mollieClient.ts

--- mollieClient.ts
+++ mollieClient.ts
@@ -1,6 +1,10 @@
+// mollieClient.ts
 import createMollieClient from "@mollie/api-client";
-console.log("Mollie API Key:", process.env.MOLLIE_API_KEY);
+
+// Node env types erzwingen
+// (verhindert: "Cannot find name 'process'")
+declare const process: any;
 
 export const mollie = createMollieClient({
-  apiKey: process.env.MOLLIE_API_KEY!
+  apiKey: process.env.MOLLIE_API_KEY,
 });
